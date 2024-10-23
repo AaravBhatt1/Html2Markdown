c0cd0ada7882078f2228a2cd386418e4edd45bb1
Aarav Bhatt <aaravbhatt@Aaravs-MacBook-Air-3.local>
2024-10-23 16:31:56 +0100

    Added more comments (with AI)

--- _build/default/lib/reader.ml
+++ _build/default/lib/reader.ml
@@ -3,36 +3,43 @@ let getFirstLine (seq : char Seq.t) : char list * char Seq.t =
   let isNotNewLine c = c != '\n'
   in (Seq.take_while isNotNewLine seq |> List.of_seq), (Seq.drop_while isNotNewLine seq |> Seq.drop 1)
 
+(* Module for representing the start of a Markdown line *)
 module MarkdownLineStarter = struct
   type t =
     | Header of int
     | UnorderedListItem
 
+  (* Convert a MarkdownLineStarter to a string representation *)
   let to_string = function
     | Header n -> "Header " ^ string_of_int n
     | UnorderedListItem -> "UnorderedListItem"
 
+  (* Check equality of two MarkdownLineStarters *)
   let equal a b =
     match a, b with
     | Header n1, Header n2 -> n1 = n2
     | UnorderedListItem, UnorderedListItem -> true
     | _, _ -> false
 
+  (* Pretty-print a MarkdownLineStarter *)
   let pp fmt v =
     Format.fprintf fmt "%s" (to_string v)
 end
 
+(* Module for representing different types of text in Markdown *)
 module MarkdownText = struct
   type t =
     | NormalText of string
     | BoldText of string
     | ItalicText of string
 
+  (* Convert a MarkdownText to a string representation *)
   let to_string = function
     | NormalText s -> "NormalText(" ^ s ^ ")"
     | BoldText s -> "BoldText(" ^ s ^ ")"
     | ItalicText s -> "ItalicText(" ^ s ^ ")"
 
+  (* Check equality of two MarkdownTexts *)
   let equal a b =
     match a, b with
     | NormalText s1, NormalText s2 -> s1 = s2
@@ -40,13 +47,16 @@ module MarkdownText = struct
     | ItalicText s1, ItalicText s2 -> s1 = s2
     | _, _ -> false
 
+  (* Pretty-print a MarkdownText *)
   let pp fmt v =
     Format.fprintf fmt "%s" (to_string v)
 end
 
+(* Module for representing a complete Markdown line *)
 module MarkdownLine = struct
   type t = MarkdownLine of MarkdownLineStarter.t option * MarkdownText.t list
 
+  (* Convert a MarkdownLine to a string representation *)
   let to_string (MarkdownLine (starter, texts)) =
     let starter_str = match starter with
       | Some s -> MarkdownLineStarter.to_string s ^ " "
@@ -56,10 +66,12 @@ module MarkdownLine = struct
     in
     "MarkdownLine(" ^ starter_str ^ "[" ^ texts_str ^ "])"
 
+  (* Check equality of two MarkdownLines *)
   let equal (MarkdownLine (s1, t1)) (MarkdownLine (s2, t2)) =
     Option.equal MarkdownLineStarter.equal s1 s2
     && List.equal MarkdownText.equal t1 t2
 
+  (* Pretty-print a MarkdownLine *)
   let pp fmt (MarkdownLine (starter, texts)) =
     Format.fprintf fmt "MarkdownLine(%a, %a)"
       (Format.pp_print_option MarkdownLineStarter.pp) starter
@@ -77,17 +89,20 @@ let parseLineStart = function
   | ' ' :: '*' :: ' ' :: xn -> Some (MarkdownLineStarter.UnorderedListItem), xn
   | xn -> None, xn
 
+(* Module for representing different types of tokens in Markdown text *)
 module TextToken = struct
   type t =
     | RegularTextToken of string
     | BoldToken
     | ItalicToken
 
+  (* Convert a TextToken to a string representation *)
   let to_string = function
     | RegularTextToken s -> "RegularTextToken(" ^ s ^ ")"
     | BoldToken -> "BoldToken"
     | ItalicToken -> "ItalicToken"
 
+  (* Check equality of two TextTokens *)
   let equal a b =
     match a, b with
     | RegularTextToken s1, RegularTextToken s2 -> s1 = s2
@@ -95,23 +110,27 @@ module TextToken = struct
     | ItalicToken, ItalicToken -> true
     | _, _ -> false
 
+  (* Pretty-print a TextToken *)
   let pp fmt v =
     Format.fprintf fmt "%s" (to_string v)
 end
 
-(* This function should tokenize the text in the line *)
+(* This function tokenizes the text in the line *)
 let tokenizeText text =
+  (* Convert characters to tokens *)
   let rec tokenize = function
     | '*' :: '*' :: xn -> TextToken.ItalicToken :: tokenize xn
     | '*' :: xn -> TextToken.BoldToken :: tokenize xn
     | x :: xn -> TextToken.RegularTextToken (Char.escaped x) :: tokenize xn
     | [] -> []
+  (* Combine adjacent RegularTextTokens *)
   in let rec combine = function
     | TextToken.RegularTextToken a :: TextToken.RegularTextToken b :: xn -> combine (TextToken.RegularTextToken (a ^ b) :: xn)
     | x :: xn -> x :: combine xn
     | [] -> []
   in text |> tokenize |> combine
 
+(* This function parses tokens into MarkdownText *)
 let rec parseTokens =
   let open TextToken
   in function
@@ -121,18 +140,21 @@ let rec parseTokens =
     | [] -> []
     | _ -> failwith "Parsing Error"
 
+(* This function parses a single line of Markdown *)
 let parseLine line =
   let starter, rest = parseLineStart line
   in let tokenizedText = tokenizeText rest
   in let parsedText = parseTokens tokenizedText
   in MarkdownLine.MarkdownLine (starter, parsedText)
 
+(* This function converts a sequence of characters to a sequence of lines *)
 let rec convertSeqCharToSeqLine charSeq = match charSeq () with
   | Seq.Nil -> Seq.empty
   | _ -> match getFirstLine charSeq with
     | line, rest -> Seq.cons line (convertSeqCharToSeqLine rest)
 
 (* TODO: unit test this function *)
+(* This function parses the entire Markdown text *)
 let parseText text = text
   |> convertSeqCharToSeqLine
   |> (Seq.map parseLine)
